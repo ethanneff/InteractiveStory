1507d3847f94ea24dec0a5d4b4f96ad5d58c33ec
Ethan Neff <ethan.neff@eneff.com>
2016-04-22 10:29:39 -0700

    interactive story model complete

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    let story = Page(story: .TouchDown)
+    story.firstChoice = (title: "some Title", page: Page(story: .Droid))
+    story.firstChoice = (title: "some Title", page: Page(story: .Droid))
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
